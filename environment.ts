3eae3f5fb7b34f2b9e3273aaf22dbccb0b16b091
Rekha Narang <rekhanarang@Rekhas-MacBook-Air.local>
2025-07-06 19:11:40 +0530

    [Updated] URLs

--- environment.ts
+++ environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  // BASE_URL: 'https://resume-screener-backend-zelt.onrender.com', 
-  // NODE_BASE_URL: 'https://resume-screener-node-backend-1-87al.onrender.com',
+  BASE_URL: 'https://resume-screener-backend-zelt.onrender.com', 
+  NODE_BASE_URL: 'https://resume-screener-node-backend-1-87al.onrender.com',
   GOOGLE_CLIENT_ID: '313220305990-vn8rl46vico4b60cgfr3uontk2q0ll90.apps.googleusercontent.com',
-  NODE_BASE_URL: 'http://localhost:3000',
-  BASE_URL: 'http://localhost:8000', 
+  // NODE_BASE_URL: 'http://localhost:3000',
+  // BASE_URL: 'http://localhost:8000', 
 }
