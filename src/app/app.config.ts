735d78d7465ba1e57d1974e6ffce4605c165c42f
Rekha Narang <rekhanarang@Rekhas-MacBook-Air.local>
2025-07-14 03:18:13 +0530

    [Added] Analtyics seperate component, remove from config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,13 @@
-import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom, PLATFORM_ID, inject } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthService } from './services/auth/auth.service';
 import { firstValueFrom } from 'rxjs';
-import { isPlatformBrowser } from '@angular/common';
-import { inject as injectAnalytics } from '@vercel/analytics';
 
 export function initializeApp(authService: AuthService) {
   return () => firstValueFrom(authService.checkAuthStatus());
-}
-
-export function initializeAnalytics(): () => void {
-  return () => {
-    const platformId = inject(PLATFORM_ID);
-    if (isPlatformBrowser(platformId)) {
-      injectAnalytics();
-    }
-  };
 }
 
 export const appConfig: ApplicationConfig = {
@@ -29,11 +18,6 @@ export const appConfig: ApplicationConfig = {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
       deps: [AuthService],
-      multi: true
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeAnalytics,
       multi: true
     },
     provideHttpClient(withInterceptorsFromDi())
